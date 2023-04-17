0880befa0180e6b23c7cdd7f2d6861cf56a5fb3b
abraham2212 <ibrahimsak@code.edu.az>
2023-04-17 21:57:20 +0400

    task

---
+++ Ajax, Services/Program.cs
@@ -0,0 +1,41 @@
+using Practice.Data;
+using Practice.Services.Interfaces;
+using Practice.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddSession();
+builder.Services.AddDbContext<AppDbContext>(option =>
+option.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddScoped<IBasketService, BasketService>();
+builder.Services.AddScoped<ILayoutService, LayoutService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ Ajax, Services/ViewModels/Layout/LayoutVM.cs
@@ -0,0 +1,10 @@
+﻿namespace Practice.ViewModels.Layout
+{
+    public class LayoutVM
+    {
+        public Dictionary<string,string> Settings { get; set; }
+        public int  BasketCount { get; set; }
+
+        //public decimal GrandTotal { get; set; }
+    }
+}
